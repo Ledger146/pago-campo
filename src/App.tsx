3e84780ea1a26a1d104ea3f9d606b759ddb9f4ff
Ledger146 <ledger.146@gmail.com>
2026-09-07 20:32:03 -0500

    feat: route PagoCampo MVP demo

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import AdminDashboard from "./pages/AdminDashboard";
+import Mvp from "./pages/Mvp";
 import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
@@ -18,7 +19,7 @@ const App = () => (
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/admin" element={<AdminDashboard />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+          <Route path="/mvp" element={<Mvp />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
